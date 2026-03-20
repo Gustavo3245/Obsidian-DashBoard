f54b6860c3ca96cb5f6d1ef0f3556b0930758d51
Gustavo3245 <Gustavomoreiradev3245@gmail.com>
2026-03-20 17:48:26 -0300

    Feat: Implementing new ways of return value (string/number)

--- src/services/VaultService.tsx
+++ src/services/VaultService.tsx
@@ -1,4 +1,4 @@
-import { App, getAllTags } from "obsidian";
+import { App, getAllTags, FrontMatterCache, CachedMetadata } from "obsidian";
 
 export class VaultService {
 	constructor(private app: App) {}
@@ -17,8 +17,7 @@ export class VaultService {
 	}
 
 	// getMarkdownFiles retorna uma Promise, que é uma lista composta de TFile (obsdian markDown File)
-	// o intuito aqui é mapear cada arquivo e em cada arquivo realizar a operação necessaria da função
-	//
+	// o intuito aqui é mapear cada arquivo e em cada arquivo realizar a operação necessaria da função.
 
 	async calculateTotalword(): Promise<number> {
 		const files = this.app.vault.getMarkdownFiles();
@@ -26,7 +25,7 @@ export class VaultService {
 		const wordCount = await Promise.all(
 			files.map(async (file) => {
 
-				// spilt the actual content of this complete file created a list
+				// split the actual content of this complete file created a list
 				// where every item divide by a space/colon or ponctuation is a item
 				// ifs the length of the item is > 2 caracters is considered a word.
 				const content = await this.app.vault.read(file);
@@ -37,7 +36,8 @@ export class VaultService {
 		return wordTotal;
 	}
 
-	getMostAppearsTagInContent(): String | null {
+	// FIXME
+	getMostAppearsTagInAllContent(): string | number {
 		const files = this.app.vault.getMarkdownFiles();
 		const tagCount: Record<string, number> = {};
 
@@ -55,10 +55,41 @@ export class VaultService {
 			}
 		}
 
-		const mostAppearsTag = Object.entries(tagCount).sort((a, b) => b[1] - a[1]);
-		return mostAppearsTag.length > 0 ? mostAppearsTag[0][0] : null;
+		const mostAppearsTag = Object.entries(tagCount).sort((current, previous) => current[1] - previous[1]);
+		return mostAppearsTag.length > 0 ? mostAppearsTag[0][1] : "Nothing but Wind";
 		
 	}
 
+	// FIXME
+	getMostAppearsTagInFrontMatter(): String | number {
+		const files = this.app.vault.getMarkdownFiles();
+		const tagCount: Record<string, number> = {};
 
+		for (const file of files){ 
+			const cache = this.app.metadataCache.getFileCache(file);
+
+			if(cache?.frontmatter && cache.frontmatter.tags) {
+				let tags = cache.frontmatter.tags;
+
+				// a tag pode ser storage dentro do obsidian como uma string tag: "model" ou
+				// um array de tags: [model, backend].
+				if(typeof tags === 'string') {
+					tags = [tags];
+				}
+
+				// a declaração de tag dentro do frontmatter é feita por (# + nameTag)
+				// O problema está que é possível declarar sem usar o #.
+				if(Array.isArray(tags)) {
+					tags.forEach(tag => {
+						const formattedTag = tag.startsWith('#') ? tag : `#{tag}`;
+						tagCount[formattedTag] = (tagCount[formattedTag] || 0) + 1;
+					});
+				}
+			}
+		}
+		// transforma o array em um Array de arrays, compara cada array (current) com o proximo (previous) retornando
+		// o array que possue a maior aparição.
+		const mostAppearsTag = Object.entries(tagCount).sort((current, previous) => current[1] - previous[1]);
+		return mostAppearsTag.length > 0 ? mostAppearsTag[0][1] : "Nothing but Wind"
+	}
 }
