b3e76395e08feb9104c30d168655c29c1ea2860a
Gustavo3245 <gustavomoreiradev3245@gmail.com>
2026-03-28 23:54:59 -0300

    Feat: Writing the first function with commands

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,25 @@ import {DEFAULT_SETTINGS, MyPluginSettings, SampleSettingTab} from "./settings";
 
 import { VaultService } from "./services/VaultService";
 import { StatProcessor } from 'services/StatsProcessor';
-import { Logger } from 'utils/Logger';
+import { VaultCommands } from 'commands/VaultCommands';
+
 
 // Remember to rename these classes and interfaces!
 export default class DashboardPlugin extends Plugin {
-	vaultService: VaultService = new VaultService(this.app);
-	statsProcessor: StatProcessor = new StatProcessor(this.vaultService);
+
+	private vaultService: VaultService;
+	private statsProcessor: StatProcessor;
+	private vaultCommands: VaultCommands ;
 
 	async onload() {
+
+		this.vaultService = new VaultService(this.app);
+		this.statsProcessor = new StatProcessor(this.vaultService);
+		
+		this.vaultCommands = new VaultCommands(this, this.statsProcessor);
+
+
+		this.vaultCommands.register();
 		console.log("Plugin is load")
 		const processor = await this.statsProcessor.getSnapshot('all');
 		console.log(`the total character: ${processor.totalCharacters}`);
